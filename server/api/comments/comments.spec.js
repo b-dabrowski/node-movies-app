12772af759030e439d38615d3604fcc1682e296c
Bart <daabrowski.baartlomiej@gmail.com>
2018-09-13 13:13:01 +0200

    Add comments tests

--- server/api/comments/comments.spec.js
+++ server/api/comments/comments.spec.js
@@ -2,13 +2,14 @@
 /* eslint prefer-destructuring: 0 */
 /* eslint no-underscore-dangle: 0 */
 /* eslint no-shadow: 0 */
+/* eslint no-unused-expressions: 0 */
 
 const expect = require('chai').expect;
 const request = require('supertest');
 const app = require('../../server');
 
 describe('[Comments]', () => {
-  it('should return the error if there is no movie with that id', (done) => {
+  it('should return the error if there is no movieId in request', (done) => {
     request(app)
       .post('/api/comments')
       .send({})
@@ -17,8 +18,57 @@ describe('[Comments]', () => {
       .expect(422)
       .end((err, resp) => {
         expect(resp.body.errors).to.be.an('object');
-        expect(resp.body.errors).has.property('id');
-        expect(resp.body.errors).property('id').eq('Id parameter is required');
+        expect(resp.body.errors).has.property('movieId');
+        expect(resp.body.errors).property('movieId').eq('Movie id parameter is required');
+        done();
+      });
+  });
+
+  it('should return the error if there is no comments text in request', (done) => {
+    request(app)
+      .post('/api/comments')
+      .send({
+        movieId: '53cb6b9b4f4ddef1ad47f943',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .end((err, resp) => {
+        expect(resp.body.errors).to.be.an('object');
+        expect(resp.body.errors).has.property('comments');
+        expect(resp.body.errors).property('comments').eq('Comments text parameter is required');
+        done();
+      });
+  });
+
+  it('should return the empty array if there is no movie with that id', (done) => {
+    request(app)
+      .get('/api/comments')
+      .send({
+        movieId: '53cb6b9b4f4ddef1ad47f943',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, resp) => {
+        expect(resp.body.comments).to.be.an('array').that.is.empty;
+        done();
+      });
+  });
+
+  it('should return error if there is no valid movie id', (done) => {
+    request(app)
+      .get('/api/comments')
+      .send({
+        movieId: 'magicNumber',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .end((err, resp) => {
+        expect(resp.body.errors).to.be.an('object');
+        expect(resp.body.errors).has.property('movieId');
+        expect(resp.body.errors).property('movieId').eq('Movie id parameter is invalid');
         done();
       });
   });
@@ -31,18 +81,18 @@ describe('[Comments]', () => {
       })
       .set('Accept', 'application/json')
       .end((err, resp) => {
-        const movie = resp.body.user.movie;
+        const movie = resp.body.movie;
 
         request(app)
           .post('/api/comments')
           .send({
-            movie: movie._id,
+            movieId: movie._id,
             text: 'test comment',
           })
           .end((err, resp) => {
             expect(resp.body.comment).to.be.an('object');
-            expect(resp.body.comment).has.property('text');
-            expect(resp.body.comment).property('text').eq('test comment');
+            expect(resp.body.comment).has.property('Text');
+            expect(resp.body.comment).property('Text').eq('test comment');
             done();
           });
       });
@@ -55,17 +105,17 @@ describe('[Comments]', () => {
       .expect('Content-Type', /json/)
       .expect(200)
       .end((err, resp) => {
-        const movie = resp.body.movies[0];
+        const movie = resp.body.movies.slice(-1).pop();
 
         request(app)
           .get('/api/comments')
           .send({
-            movie: movie._id,
+            movieId: movie._id,
           })
           .end((err, resp) => {
             expect(resp.body.comments).to.be.an('array');
-            expect(resp.body.comments).has.property('movie');
-            expect(resp.body.comments).property('movie').eq(movie._id);
+            expect(resp.body.comments[0]).has.property('Movie');
+            expect(resp.body.comments[0]).property('Movie').eq(movie._id);
             done();
           });
       });
@@ -77,8 +127,8 @@ describe('[Comments]', () => {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end((err, resp) => {
-        expect(resp.body.comments).to.be.an('array');
+      .end((err, resp) => {        
+        expect(resp.body.comments).to.be.an('array').that.is.not.empty;
         done();
       });
   });
