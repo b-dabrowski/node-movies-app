35c4cd7bece83878c49bc5adf351df019773910f
Bart <daabrowski.baartlomiej@gmail.com>
2018-09-14 13:21:32 +0200

    Add swagger

--- server/api/api.js
+++ server/api/api.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../util/swagger.json');
 const moviesRouter = require('./movies/moviesRoutes');
 const commentsRouter = require('./comments/commentsRoutes');
 
 router.use('/movies', moviesRouter);
 router.use('/comments', commentsRouter);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
